dee3ecc880f8f3d43da139e7dfa551e72799e3b3
Samuel Oliveira da Silva <samosaara@gmail.com>
2024-10-08 23:33:50 +0200

    fix: removes cloudflare

--- app.config.ts
+++ app.config.ts
@@ -2,12 +2,6 @@ import UnoCSS from "unocss/vite";
 import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
-  server: {
-    preset: "cloudflare-pages",
-    rollupConfig: {
-      external: ["node:async_hooks", "@gcornut/valibot-json-schema"],
-    },
-  },
   vite: {
     plugins: [UnoCSS()],
   },
